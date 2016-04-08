28890b0ec65b0e1184c626208a9656b49cd07301
Chris Burgess <chris@giantrobot.co.nz>
2016-04-08 23:37:57 +1200

    Rewrite, position labels nicely.

--- graph.js
+++ graph.js
@@ -6,37 +6,94 @@
 // Water: 55-65
 // Bone %ge: 3.9%
 // Weight: 90-75
-d3.json("data/data.json", function(error, data) {
-  // console.log(data.data.rows);
+var bodyStats = {
+  graph: {
+    margin: {
+      top: 30,
+      right: 75,
+      bottom: 35,
+      left: 50
+    },
 
-  // Data prep: ignore entries by timestamp.
-  var ignoreTimeStamps = [ 1457869810 ];
-  data.data.rows = data.data.rows.filter(function(d) {
-    return ignoreTimeStamps.indexOf(d.timeStamp) === -1;
-  });
+    width: 960,
+    height: 470
+  },
 
-  // Data prep: get JS dates from input string.
-  data.data.rows.forEach(function(d) {
+  ignoreTimestamps: [
+    1457869810
+  ],
+
+  filterData: function(d) {
+    return bodyStats.ignoreTimestamps.indexOf(d.timeStamp) === -1;
+  },
+
+  prepRow: function(d) {
     d.date = d3.time.format("%Y-%m-%d %H:%M:%S").parse(d.createTime);
+    // d.date = d3.time.format("%Y-%m-%d %H:%M:%S").parse(d.date.toISOString().substring(0, 10) + ' 12:00:00');
     d.bmi = +d.bmi;
     d.bmr = +d.bmr;
     d.fat = +d.fat;
     d.water = +d.water;
     d.weight = +d.weight;
-  });
+  },
+
+  adjustFinal: function(d) {
+    console.log(d);
+  },
+
+  drawGraph: function() {
+
+
+  }
+};
+
+d3.json("data/data.json", function(error, data) {
+  // Data prep: ignore entries by timestamp.
+  data.data.rows = data.data.rows.filter(bodyStats.filterData);
+
+  // Data prep: get JS dates from input string.
+  data.data.rows.forEach(bodyStats.prepRow);
 
   // Set the dimensions of the canvas / graph
-  var margin = {top: 30, right: 75, bottom: 35, left: 50},
-  width = 960 - margin.left - margin.right,
-  height = 470 - margin.top - margin.bottom;
+  var margin = bodyStats.graph.margin,
+    width = bodyStats.graph.width - bodyStats.graph.margin.left - bodyStats.graph.margin.right,
+    height = bodyStats.graph.height - bodyStats.graph.margin.top - bodyStats.graph.margin.bottom;
+
+  var svg = d3.select("body")
+    .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .style('margin', '0 auto')
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+  svg.append("rect")
+    .attr("x", 0)
+    .attr("y", 0)
+    .attr("width", width)
+    .attr("height", height/4)
+    .style('fill', 'rgb(255, 235, 211)')
+    .style('opacity', 0.5);
+
+  svg.append("rect")
+    .attr("x", 0)
+    .attr("y", height/4*3)
+    .attr("width", width)
+    .attr("height", height/4)
+    .style('fill', 'rgb(211, 255, 211)')
+    .style('opacity', 0.5);
 
   // Set the ranges
   var x = d3.time.scale().range([0, width]);
   var yBMI = d3.scale.linear().range([height, 0]);
   var yBMR = d3.scale.linear().range([height, 0]);
   var yFat = d3.scale.linear().range([height, 0]);
-  var yWater = d3.scale.linear().range([0, height]);
+  var yVisFat = d3.scale.linear().range([height, 0]);
+  var yWater = d3.scale.linear().range([height, 0]);
   var yWeight = d3.scale.linear().range([height, 0]);
+
+  // Scale the range of the data
+  x.domain(d3.extent(data.data.rows, function(d) { return d.date; }));
 
   // Define the axes
   var xAxis = d3.svg.axis().scale(x)
@@ -49,6 +106,9 @@ d3.json("data/data.json", function(error, data) {
     .orient("right")
     .ticks(2);
   var yAxisFat = d3.svg.axis().scale(yFat)
+    .orient("right")
+    .ticks(5);
+  var yAxisVisFat = d3.svg.axis().scale(yVisFat)
     .orient("right")
     .ticks(5);
   var yAxisWater = d3.svg.axis().scale(yWater)
@@ -83,6 +143,14 @@ d3.json("data/data.json", function(error, data) {
     .y(function(d) {
       return yFat(d.fat);
     });
+  var valuelineVisFat = d3.svg.line()
+    .interpolate('basis')
+    .x(function(d) {
+      return x(d.date);
+    })
+    .y(function(d) {
+      return yVisFat(d.visFat);
+    });
   var valuelineWater = d3.svg.line()
     .interpolate('basis')
     .x(function(d) {
@@ -100,34 +168,6 @@ d3.json("data/data.json", function(error, data) {
       return yWeight(d.weight);
     });
 
-  // Adds the svg canvas
-  var svg = d3.select("body")
-    .append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-    .style('margin', '0 auto')
-    .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-  svg.append("rect")
-    .attr("x", 0)
-    .attr("y", 0)
-    .attr("width", width)
-    .attr("height", height/4)
-    .style('fill', 'rgb(255, 235, 211)')
-    .style('opacity', 0.5);
-
-  svg.append("rect")
-    .attr("x", 0)
-    .attr("y", height/4*3)
-    .attr("width", width)
-    .attr("height", height/4)
-    .style('fill', 'rgb(211, 255, 211)')
-    .style('opacity', 0.5);
-
-  // Scale the range of the data
-  x.domain(d3.extent(data.data.rows, function(d) { return d.date; }));
-
   // console.log([0.95 * d3.min(data.data.rows, function(d) { return d.bmr; }), 1.05 * d3.max(data.data.rows, function(d) { return d.bmr; })], 'bmr');
   // console.log([0.95 * d3.min(data.data.rows, function(d) { return d.fat; }), 1.05 * d3.max(data.data.rows, function(d) { return d.fat; })], 'fat');
   // console.log([0.99 * d3.min(data.data.rows, function(d) { return d.water; }), 1.01 * d3.max(data.data.rows, function(d) { return d.water; })]);
@@ -138,6 +178,9 @@ d3.json("data/data.json", function(error, data) {
   yFat.domain([5, 34]);
   yWater.domain([50, 70]);
   yWeight.domain([80, 88]);
+
+  // console.log(data.data.rows, 'data');
+  // console.log(valuelineBMI(data.data.rows), 'BMI');
 
   // Add the valueline paths.
   svg.append("path")
@@ -169,49 +212,75 @@ d3.json("data/data.json", function(error, data) {
     .style('stroke-width', 2)
     .attr("d", valuelineWeight(data.data.rows));
 
-  // console.log(valuelineWater(data.data.rows));
-
-  console.log(yWater(data.data.rows[data.data.rows.length-1].water), 'water');
-  console.log(yBMI(data.data.rows[data.data.rows.length-1].bmi), 'bmi');
-
-  var final = {
-    water: data.data.rows[data.data.rows.length-1].water,
-    bmi: data.data.rows[data.data.rows.length-1].bmi,
-    bmr: data.data.rows[data.data.rows.length-1].bmr,
-    fat: data.data.rows[data.data.rows.length-1].fat,
-    weight: data.data.rows[data.data.rows.length-1].weight
+  bodyStats.final = {
+    water: {
+      label: 'Water: ' + data.data.rows[data.data.rows.length-1].water + '%',
+      value: yWater(data.data.rows[data.data.rows.length-1].water),
+    },
+    bmi: {
+      label: 'BMI: ' + data.data.rows[data.data.rows.length-1].bmi.toFixed(1),
+      value: yBMI(data.data.rows[data.data.rows.length-1].bmi)
+    },
+    bmr: {
+      label: 'BMR: ' + data.data.rows[data.data.rows.length-1].bmr.toFixed(1),
+      value: yBMR(data.data.rows[data.data.rows.length-1].bmr)
+    },
+    fat: {
+      label: 'Fat: ' + data.data.rows[data.data.rows.length-1].fat.toFixed(1) + '%',
+      value: yFat(data.data.rows[data.data.rows.length-1].fat)
+    },
+    weight: {
+      label: 'Weight: ' + data.data.rows[data.data.rows.length-1].weight.toFixed(1),
+      value: yWeight(data.data.rows[data.data.rows.length-1].weight)
+    }
   };
 
+  // Get position order for the labels.
+  var finals = Object.keys(bodyStats.final);
+  finals.sort(function(a, b) { return bodyStats.final[a].value - bodyStats.final[b].value });
+  // Ensure a minimum of 10px between each label.
+  var prev = bodyStats.final[finals[0]].value;
+  finals.forEach(function (key, i) {
+    // Compare against previous, so skip 0th.
+    if (i && bodyStats.final[key].value < (prev + 10)) {
+      bodyStats.final[finals[i-1]].value += 8;
+      bodyStats.final[key].value -= 8;
+    }
+    prev = bodyStats.final[finals[i]].value;
+  });
+
+  console.log(finals);
+
   svg.append('text')
-    .attr('transform', 'translate(' + (width + 3) + ',' + yWater(data.data.rows[data.data.rows.length-1].water) + ')')
+    .attr('transform', 'translate(' + (width + 3) + ',' + bodyStats.final.water.value + ')')
     .attr('dy', '.35em')
     .attr('text-anchor', 'start')
     .style('fill', 'blue')
-    .text('Water: ' + final.water + '%');
+    .text(bodyStats.final.water.label);
   svg.append('text')
-    .attr('transform', 'translate(' + (width + 3) + ',' + yBMI(data.data.rows[data.data.rows.length-1].bmi) + ')')
+    .attr('transform', 'translate(' + (width + 3) + ',' + bodyStats.final.bmi.value + ')')
     .attr('dy', '.35em')
     .attr('text-anchor', 'start')
     .style('fill', 'red')
-    .text('BMI: ' + final.bmi);
+    .text(bodyStats.final.bmi.label);
   svg.append('text')
-    .attr('transform', 'translate(' + (width + 3) + ',' + yBMR(data.data.rows[data.data.rows.length-1].bmr) + ')')
+    .attr('transform', 'translate(' + (width + 3) + ',' + bodyStats.final.bmr.value + ')')
     .attr('dy', '.35em')
     .attr('text-anchor', 'start')
     .style('fill', 'grey')
-    .text('BMR: ' + final.bmr);
+    .text(bodyStats.final.bmr.label);
   svg.append('text')
-    .attr('transform', 'translate(' + (width + 3) + ',' + yFat(data.data.rows[data.data.rows.length-1].fat) + ')')
+    .attr('transform', 'translate(' + (width + 3) + ',' + bodyStats.final.fat.value + ')')
     .attr('dy', '.35em')
     .attr('text-anchor', 'start')
     .style('fill', 'green')
-    .text('Fat: ' + final.fat + '%');
+    .text(bodyStats.final.fat.label + '%');
   svg.append('text')
-    .attr('transform', 'translate(' + (width + 3) + ',' + yWeight(data.data.rows[data.data.rows.length-1].weight) + ')')
+    .attr('transform', 'translate(' + (width + 3) + ',' + bodyStats.final.weight.value + ')')
     .attr('dy', '.35em')
     .attr('text-anchor', 'start')
     .style('fill', 'black')
-    .text('Weight: ' + final.weight);
+    .text(bodyStats.final.weight.label);
 
   // Add the X Axis
   svg.append("g")
@@ -260,13 +329,4 @@ d3.json("data/data.json", function(error, data) {
     .style('font-size', '14px')
     .text('Date');
 
-/*
-  var p = d3.select("body").selectAll("p")
-    .data(data.data.rows)
-    .enter()
-    .append("p")
-    .text(function(measure) {
-      return measure.createTime + ': ' + measure.bmi;
-    });
-  */
 })
